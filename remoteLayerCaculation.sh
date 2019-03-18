46ad46143f09852ec2a60f38fe7c80e1a9aff919
Alon Elmaliah <alon.elmaliah@gmail.com>
2019-03-18 14:31:18 +0200

    test2

--- remoteLayerCaculation.sh
+++ remoteLayerCaculation.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd docs/private_tangle
-mkdir remote && cd remote
 
+ROOT_TREE_LOCAL=`cat data/layers/layer.0.csv`
 echo "starting Signing server"
 ./11_run_signature_source_server.sh &
 sleep 10
@@ -10,10 +10,13 @@ echo "starting remote layer calculation"
 ./21_calculate_layers_remote.sh
 
 echo "compare results"
+ROOT_TREE_REMOTE=`cat data/layers/layer.0.csv`
 diff ../data/layers/layer.0.csv data/layers/layer.0.csv
-if diff -q ../data/layers/layer.0.csv data/layers/layer.0.csv &>/dev/null; then
-  >&2 echo "same"
+if [ ${ROOT_TREE_REMOTE} = ${ROOT_TREE_LOCAL} ]; then
+  >&2 echo "same";
   exit -1
+else
+  >&2 echo "different: ${ROOT_TREE_REMOTE} != ${ROOT_TREE_LOCAL}"
 fi
 
 echo "cleaning up"
