867aa50f42ad5b17d00466927254a5e2490d14a5
Alon Elmaliah <alon.elmaliah@gmail.com>
2019-03-18 14:08:00 +0200

    add remote layer calculation test

---
+++ remoteLayerCaculation.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+cd docs/private_tangle
+
+echo "starting Signing server"
+./11_run_signature_source_server.sh &
+sleep 10
+
+echo "starting remote layer calculation"
+./21_calculate_layers_remote.sh
+
+echo "compare results"
+diff ../data/layers/layer.0.csv data/layers/layer.0.csv
+if ! diff -q ../data/layers/layer.0.csv data/layers/layer.0.csv &>/dev/null; then
+  >&2 echo "different"
+fi
+
+echo "cleaning up"
+docker kill $(docker ps | grep signature_source_server | cut -f1 -d\ )
